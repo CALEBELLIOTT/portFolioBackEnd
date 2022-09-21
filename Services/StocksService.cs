0d98186bde133face8980ec5da980fe8ff288c09
Caleb Elliott <caleb123elliott@gmail.com>
2022-09-21 13:04:02 -0600

    broken :(

--- Services/StocksService.cs
+++ Services/StocksService.cs
@@ -1,7 +1,31 @@
+using Microsoft.AspNetCore.Mvc;
+using portFolioBackEnd.Repositories;
+using portFolioBackEnd.Models;
+using System.Collections.Generic;
+
+
 namespace portFolioBackEnd.Services
 {
-    public class StocksService
-    {
+  public class StocksService
+  {
+    private readonly StocksRepository _repo;
 
+    public StocksService(StocksRepository repo)
+    {
+      _repo = repo;
     }
+
+    internal List<Stock> Get()
+    {
+      try
+      {
+        List<Stock> stocks = _repo.get();
+        return stocks;
+      }
+      catch (System.Exception e)
+      {
+        throw new System.Exception(e.Message);
+      }
+    }
+  }
 }
